2b49f62f70c0a9306ed0f7ddf5a95c2aebf0e884
TADstudent <markus84848@googlemail.com>
2020-11-17 22:16:12 +0100

    Created interface and scripts for ships/asteroids to destroy them. Need to think about implementation of Ships in general together with others. Ship has 100 hp, space calls function to deal 10 damage. At 0 hp Ship disappears. Asteroid disapears with press of backspace.

---
+++ Assets/AsteroidDestruction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AsteroidDestruction : MonoBehaviour, IEntity
+{
+    public float health { get; set; }
+    public float maxHealth = 1;
+    public GameObject asteroid;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            this.ApplyDamage(10);
+        }
+    }
+
+    private void Awake()
+    {
+        this.Initialize();
+    }
+
+
+    public void Initialize()
+    {
+        this.health = this.maxHealth;
+    }
+
+    public void ApplyDamage(float points)
+    {
+            Object.Destroy(this.asteroid);
+    }
+}
---
+++ Assets/IEntity.cs
@@ -0,0 +1,6 @@
+﻿public interface IEntity
+{
+    void Initialize(); //Function without any arguments
+    float health { get; set; } //A variable
+    void ApplyDamage(float points); //Function with one argument
+}
