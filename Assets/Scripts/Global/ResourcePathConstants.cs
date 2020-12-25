e0c677909bdba6e1d94e1031ae76acb9e90f7439
patrick <paknei@gmx.net>
2020-12-25 19:15:27 +0100

    refactored folder structure, small bug fixes

---
+++ Assets/Scripts/Global/ResourcePathConstants.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ResourcePathConstants
+{
+
+    public static string[] BACKGROUND_SCENES = {
+        "Backgrounds/Background_1",
+        "Backgrounds/Background_2",
+        "Backgrounds/Background_3",
+        "Backgrounds/Background_4",
+        "Backgrounds/Background_5",
+        "Backgrounds/Background_6",
+        "Backgrounds/Background_7",
+        "Backgrounds/Background_8"
+    };
+
+    public static string[] ASTEROIDS = {
+        "Asteroids/Asteroid_1",
+        "Asteroids/Asteroid_2",
+        "Asteroids/Asteroid_3",
+        "Asteroids/Asteroid_4",
+        "Asteroids/Asteroid_5",
+        "Asteroids/Asteroid_6"
+    };
+
+    public static string[] PLANETS = {
+        "Planets/Planet_1",
+        "Planets/Planet_2",
+        "Planets/Planet_3",
+        "Planets/Planet_4",
+        "Planets/Planet_5",
+        "Planets/Planet_6",
+        "Planets/Planet_7",
+        "Planets/Planet_8",
+        "Planets/Planet_9"
+    };
+
+    public static string[] PLANET_SHADOWS = {
+        "Planets/Shadows/Shadow_1",
+        "Planets/Shadows/Shadow_2",
+        "Planets/Shadows/Shadow_3",
+        "Planets/Shadows/Shadow_4",
+        "Planets/Shadows/Shadow_5",
+        "Planets/Shadows/Shadow_6"
+    };
+
+
+
+    public static string SHIP_MARS = "Ships/Ship_Mars";
+
+    public static string SHIP_EARTH = "Ships/Ship_Earth";
+
+    public static string SUPPORT_SHIP = "Ships/Support_Ship";
+
+    public static string MISSILE = "ShootAndDestroy/Missile";
+
+    public static string MACHINE_GUN = "ShootAndDestroy/Projectile";
+
+    public static string explosion = "ShootAndDestroy/Explosion";
+    //public static string missile = "Prefabs/Destruction/Missile (1)";
+    //public static string asteroid = "Prefabs/Destruction/Asteroid 5";
+    //public static string ship = "Prefabs/Destruction/Frigate 2";
+    public static string debris = "ShootAndDestroy/Debris";
+
+    public static string DROP_ITEM = "Ships/Drop_Item";
+
+    //public static string EVENT_CONTROL = "EventController";
+}
