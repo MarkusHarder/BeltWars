33c8b10a8c75e7b249789192ce2fa8cb04f9acc4
Sebastian Flum <sebastian.flum@hotmail.de>
2021-01-05 15:52:04 +0100

    Added intro screen with 3 scenes and running text that explain the story of the game

---
+++ Assets/Scripts/Intro/SkipScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScene : MonoBehaviour
+{
+    public void LoadNextScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space") || Input.GetKey("escape") || Input.GetKey("enter"))
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+
+            if (currentScene == "Intro_1")
+            {
+                this.LoadNextScene("Intro_2");
+            }
+            else if (currentScene == "Intro_2")
+            {
+                this.LoadNextScene("Intro_3");
+            }
+            else if (currentScene == "Intro_3")
+            {
+                this.LoadNextScene("Menu");
+            }
+        }
+    }
+}
