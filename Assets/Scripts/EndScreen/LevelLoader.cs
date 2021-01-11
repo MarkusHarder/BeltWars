9b6c5dff7fd2ea819c5497fa74863d4f5a37982d
Sebastian Flum <sebastian.flum@hotmail.de>
2021-01-11 16:22:03 +0100

    Added Level Loader prefab with transition which loads either the loose screen oder win screen

---
+++ Assets/Scripts/EndScreen/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+ 
+    public void LoadEndScreenLoss()
+    {
+        StartCoroutine(LoadLevel("YouLoose"));
+    }
+
+    public void LoadEndScreenWin()
+    {
+        StartCoroutine(LoadLevel("YouWin"));
+    }
+
+    IEnumerator LoadLevel(string name)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load scene
+        SceneManager.LoadScene(name);
+    }
+}
