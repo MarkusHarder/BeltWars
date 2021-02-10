5b748e629bb3cd27acd2ea618614ce2f3e6209e5
TADstudent <markus84848@googlemail.com>
2021-02-10 12:56:24 +0100

    scenes are now called in network

---
+++ Assets/Scripts/Network/NetworkEndSceneHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class NetworkEndSceneHandler : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadSceneOnClient(string name)
+    {
+        if (isServer)
+        {
+            rpcSetScene(name);
+        }
+    }
+
+
+
+    [ClientRpc]
+    public void rpcSetScene(string name)
+    {
+        GlobalVariables.local = true;
+        SceneManager.LoadScene(name);
+    }
+}
